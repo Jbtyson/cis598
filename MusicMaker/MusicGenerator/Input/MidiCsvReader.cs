5e570dbb99b3d68bdd992b57a3237048affbd3c2
unknown <jbtyson@E1116W13.win.cis.ksu.edu>
2016-04-16 23:12:27 -0500

    asd

---
+++ MusicMaker/MusicGenerator/Input/MidiCsvReader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using MusicGenerator.Music;
+
+namespace MusicGenerator.Input
+{
+    public class MidiCsvReader
+    {
+        private string path;
+        private int currentTime;
+
+        public MidiCsvReader(string path)
+        {
+            this.path = path;
+        }
+
+        private IEnumerable<Note> ConvertFileToNoteList()
+        {
+            var notes = new List<Note>();
+
+            using (var sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    notes.Add(ParseInputLine(sr.ReadLine()));
+                }
+            }
+
+            return notes;
+        }
+
+        private Note ParseInputLine(string line)
+        {
+            var parts = line.Split(',');
+            var note = new Note(parts[0], currentTime, (NoteLength)int.Parse(parts[1]));
+            currentTime += int.Parse(parts[1]);
+            return note;
+        }
+
+    }
+}
